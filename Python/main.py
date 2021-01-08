7addbac9def30529940588e76e35068ab5133e17
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-08 15:23:48 -0400

    added a folder to .ignore file, created the call to an object in main.py

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,10 @@
+from car import Car
+
 if __name__ == "__main__":
     print("hello world")
+    car = Car()
+    car.id = 1
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    car.passenger = 4
+    print(vars(car))
