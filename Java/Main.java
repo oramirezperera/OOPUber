f2806750238c975c6ff4dbcedb18e8b5e9c34f08
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-08 15:08:48 -0400

    added the basic structure for PHP and changed Main.java to create two new objects car and car2

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,16 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passenger = 4;
+        System.out.println("Car License: "+ car.license);
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passenger = 3;
+        System.out.println("Car License: " + car2.license);
     }
 }
---
+++ PHP/Account.php
@@ -0,0 +1,7 @@
+class Account{
+    public $id;
+    public $document;
+    public $name;
+    public $email;
+    public $password;
+}
