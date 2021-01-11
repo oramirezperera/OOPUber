a37508b574c055de64298fceff072a895273dab1
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-11 14:11:37 -0400

    changes to the python files, now works with all the classes

--- Python/main.py
+++ Python/main.py
@@ -1,5 +1,10 @@
 from account import Account
 from car import Car
+from uberBlack import UberBlack
+from uberPool import UberPool
+from uberVan import UberVan
+from uberX import UberX
+
 
 if __name__ == "__main__":
     print("hello world")
@@ -7,3 +12,7 @@ if __name__ == "__main__":
     car = Car("AMS235", Account("Andres Herrera", "ANDA876"))
     print(vars(car))
     print(vars(car.driver))
+
+    uberX = UberX("AKSD", Account("Angie", 12356), "Chevrolet", "Spark")
+    print(vars(uberX))
+    print(vars(uberX.driver))
