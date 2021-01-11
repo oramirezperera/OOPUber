cde92920abbe8b0f1c5427a88d5d6127c5ffecfb
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-11 13:45:23 -0400

    modified index and uberx and added new objects like black pool and van

--- PHP/index.php
+++ PHP/index.php
@@ -1,5 +1,12 @@
-require_once('Car.php');
-require_once('Account.php');
+<?php
+require_once("Car.php");
+require_once("uberX.php");
+require_once("uberPool.php");
+require_once("account.php");
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->printDataCar();
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
+
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765"), "Dodge", "Attitude");
+$uberPool->printDataCar();
+?>
