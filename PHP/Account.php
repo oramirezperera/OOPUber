88d818a9b78bd0560e29663167078a55661e8fa3
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-13 10:48:15 -0400

    changes to the classes driver and user in PHP

--- PHP/Account.php
+++ PHP/Account.php
@@ -6,9 +6,11 @@ class Account{
     public $email;
     public $password;
 
-    public function __construct($name, $document){
+    public function __construct($name, $document, $email, $password){
         $this->name = $name;
         $this->document = $document;
+        $this->email = $email;
+        $this->password = $password;
     }
 }
 
--- PHP/user.php
+++ PHP/user.php
@@ -3,9 +3,7 @@ require_once("account.php");
 
 class User extends Account{
     public function __construct($name, $document, $email, $password){
-        parent::__construct($name, $document);
-        $this->email = $email;
-        $this->password = $password;
+        parent::__construct($name, $document, $email, $password);
     }
 }
 ?>
