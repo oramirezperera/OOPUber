7e49f42a29435f8174e0a5118aab0b7537560392
Orlando Ramirez <ramirez.orlando17@gmail.com>
2021-01-13 10:45:22 -0400

    created the classes user and driver

---
+++ PHP/user.php
@@ -0,0 +1,11 @@
+<?php
+require_once("account.php");
+
+class User extends Account{
+    public function __construct($name, $document, $email, $password){
+        parent::__construct($name, $document);
+        $this->email = $email;
+        $this->password = $password;
+    }
+}
+?>
